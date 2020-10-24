4f2d39b8874a16162a5e4f8adb84c4592a5b3f9e
qiyisi <qiyisi@qq.com>
2020-10-24 21:02:13 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => {
+  return (
+    <div>
+      <div>Welcome to my-webpack-react-starter</div>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
