364a862366f7e468dcd5d8ec4f063a6dd3908926
qiyisi <qiyisi@qq.com>
2020-10-26 20:47:28 +0800

    add umijs

---
+++ umijs/.umirc.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  layout: {},
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [{ path: '/', component: '@/pages/index' }],
+});
